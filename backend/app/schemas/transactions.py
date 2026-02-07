1b9fdd260c291c0ea07f324af94b5e6105ba4a30
Edrian Aldrin Mariñas <edrian.a.marinas@gmail.com>
2026-02-07 19:33:23 +0800

    init files

--- backend/app/schemas/transactions.py
+++ backend/app/schemas/transactions.py
@@ -1,11 +1,45 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from datetime import date
+from typing import Optional
+from decimal import Decimal
+
 
 class TransactionCreate(BaseModel):
-    amount: float
+    amount: Decimal = Field(gt=0, max_digits=12, decimal_places=2)
     category_id: int
     description: str
     date: date
 
 class TransactionOut(TransactionCreate):
     id: int
+
+class TransactionUpdate(BaseModel):
+    amount: Optional[Decimal] = None
+    category_id: Optional[int] = None
+    description: Optional[str] = None
+    date: Optional[date] = None
+
+class TransactionRead(TransactionOut):
+    category_name: str 
+    user_id: int       
+
+
+
+
+
+
+
+
+
+#decimal used for money, gt=0 must be greater than zero, max digits, decimal places
+
+#Optional, allows partial updates, pdeng si description lng i update
+
+"""
+Model	Used for
+TransactionCreate	POST /transactions
+TransactionUpdate	PATCH /transactions/{id}
+TransactionOut	    POST response
+TransactionRead	    GET /transactions
+
+"""
