ae452b4a435a6893cd89c965fd96b7f9916e2eae
Edrian Aldrin Mariñas <edrian.a.marinas@gmail.com>
2026-02-07 12:59:20 +0800

    Setup services folder with initial service files

--- backend/app/services/transactions_service.py
+++ backend/app/services/transactions_service.py
@@ -1,13 +1,16 @@
+# transactions_service.py → CRUD + queries + aggregation for transactions
+
+
 from db.connection import get_pool
 
 async def create_transaction(tx):
-    pool = await get_pool()
-    async with pool.acquire() as conn:
-        await conn.execute(
-            """
-            INSERT INTO transactions (amount, category_id, description, date)
-            VALUES ($1, $2, $3, $4)
-            """,
-            tx.amount, tx.category_id, tx.description, tx.date
-        )
-    return {"message": "Transaction created"}
+  pool = await get_pool()
+  async with pool.acquire() as conn:
+    await conn.execute(
+      """
+      INSERT INTO transactions (amount, category_id, description, date)
+      VALUES ($1, $2, $3, $4)
+      """,
+      tx.amount, tx.category_id, tx.description, tx.date
+    )
+  return {"message": "Transaction created"}
