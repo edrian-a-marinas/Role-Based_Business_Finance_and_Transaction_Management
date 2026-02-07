8a575b9f1e085186da6533fe053d43d4f543d1c6
Edrian Aldrin Mariñas <edrian.a.marinas@gmail.com>
2026-02-07 10:09:39 +0800

    testing

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,9 +4,14 @@ from routers import transactions, users, reports
 app = FastAPI()
 
 app.include_router(transactions.router)
-app.include_router(users.router)
-app.include_router(reports.router)
+#app.include_router(users.router)
+#app.include_router(reports.router)
 
+
+@app.get("/")
+def read_root():
+  return {"Hello": "World"}
 
 
 #uvicorn app.main:app --reload
+#http://127.0.0.1:8000/
