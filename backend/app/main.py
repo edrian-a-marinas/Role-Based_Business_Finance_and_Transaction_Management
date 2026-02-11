05998b4c8d96b2414592ebb54cf220917879d2a8
Edrian Aldrin Mariñas <edrian.a.marinas@gmail.com>
2026-02-11 16:39:04 +0800

    included users and auth routers

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 
+from app.security import router_auth as authentications
 from app.routers import (
   transactions, 
   categories, 
@@ -10,15 +11,18 @@ from app.routers import (
 
 app = FastAPI()
 
+
 # 1. Mount Static Files (CSS, JS, Images)
 # This makes files in /static accessible via ://yourdomain.com
 # app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # 2. Backend API Routings (Returns JSON)
+app.include_router(authentications.router)
+
 app.include_router(transactions.router)
 app.include_router(categories.router)
-#app.include_router(reports.router)
-#app.include_router(users.router)
+app.include_router(reports.router)
+app.include_router(users.router)
 
 
 # 3. Frontend Page Routings (Returns HTML)
