00169166d5548fa01f5bbd413137d78c1351cfc3
Edrian Aldrin Mariñas <edrian.a.marinas@gmail.com>
2026-02-18 01:48:37 +0800

    Added healthCheck and AuthProvider for login and dashboard routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,23 @@
+import { useHealthCheck } from "./services/useHealthCheck"
 import { BrowserRouter } from "react-router-dom"
+
 import Router from "./router"
+
+import { AuthProvider } from "./contexts/AuthContext"
+
 import './styles/dashboard.css'
 //import './styles/login.css'
 //import './styles/register.css'
 
 function App() {
+  useHealthCheck()
+
   return (
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </AuthProvider>
   )
 }
 
