de932f1e28a1f066af6765c53de4df1e11cdcf83
Edrian Aldrin Mariñas <edrian.a.marinas@gmail.com>
2026-02-18 01:49:28 +0800

    Routing to dashboard depends on isLoggedin or not

--- frontend/src/router/index.tsx
+++ frontend/src/router/index.tsx
@@ -1,25 +1,34 @@
-import { Routes, Route } from "react-router-dom"
+import { AuthContext } from "../contexts/AuthContext"
+import { useContext } from "react"
+
+import { Routes, Route, Navigate } from "react-router-dom"
 
 // Pages
 import Dashboard from "../pages/dashboard/Dashboard"
-// import Login from "../pages/auth/Login"
+import Login from "../pages/auth/Login"
 // import Register from "../pages/auth/Register"
 // import Transactions from "../pages/dashboard/Transactions"
 // import Categories from "../pages/dashboard/Categories"
 // import Reports from "../pages/dashboard/Reports"
 
+function RootController() {
+  const { isLoggedIn } = useContext(AuthContext)
+
+  return isLoggedIn ? <Dashboard /> : <Login /> 
+}
+
+
 export default function Router() {
+  const { isLoggedIn } = useContext(AuthContext)
+
   return (
     <Routes>
-      {/* === Prototyping homepage/dashboard === */}
-      <Route path="/" element={<Dashboard />} />
+      <Route path="/" element={<RootController />} />
 
-      {/* === Auth pages === */}
-      {/* <Route path="/login" element={<Login />} /> */}
-      {/* <Route path="/register" element={<Register />} /> */}
+      <Route path="/login" element={isLoggedIn ? <Navigate to="/" /> : <Login />} /> 
+      {/* <Route path="/register" element={isLoggedIn ? <Navigate to="/" /> : <Register />} />  */}
 
       {/* === Dashboard sub-pages (commented for now) === */}
-      {/* <Route path="/dashboard" element={<Dashboard />} /> */}
       {/* <Route path="/transactions" element={<Transactions />} /> */}
       {/* <Route path="/categories" element={<Categories />} /> */}
       {/* <Route path="/reports" element={<Reports />} /> */}
